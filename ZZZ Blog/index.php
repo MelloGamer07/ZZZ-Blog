72622f2c8e2611d5fb919805c66563503c4245db
MelloGamer07 <alessio.sky2007@gmail.com>
2026-01-17 00:13:58 +0100

    Add files via upload

---
+++ ZZZ Blog/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>ZZZ Home Page</title>
+  <style>
+    body{
+      margin:0;
+      background-image: url('ASSETS/IMG/hq720.jpg');
+      background-size: 100%;
+    }
+  </style>
+</head>
+<body>
+    <?php include 'header.html' ?>
+    <?php include 'footer.html' ?>
+</body>
+</html>
