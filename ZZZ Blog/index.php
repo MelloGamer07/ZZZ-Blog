1d2d983799a278bd89750e3932a320adfd4a111f
MelloGamer07 <alessio.sky2007@gmail.com>
2026-01-17 15:58:43 +0100

    Fafinwainoaiwnoiw

--- ZZZ Blog/index.php
+++ ZZZ Blog/index.php
@@ -9,9 +9,23 @@
       background-image: url('ASSETS/IMG/hq720.jpg');
       background-size: 100%;
     }
+
+    #DynamicWallpaper {
+      position: fixed;
+      right: 0;
+      bottom: 0;
+      min-width: 100%; 
+      min-height: 100%;
+      max-width: 100%;
+      z-index: -1;
+    }
+
   </style>
 </head>
 <body>
+    <video autoplay muted loop id="DynamicWallpaper">
+      <source src="ASSETS/DynamicWallpapers/WiseDynamicWallpaper.mp4" type="video/mp4">
+    </video>
     <?php include 'header.html' ?>
     <?php include 'footer.html' ?>
 </body>
