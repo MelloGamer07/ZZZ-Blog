675299a225f5a360c88cb2892bd242976db7bb44
MelloGamer07 <alessio.sky2007@gmail.com>
2026-01-18 17:42:17 +0100

    Updated footer

--- ZZZ Blog/index.php
+++ ZZZ Blog/index.php
@@ -15,14 +15,57 @@
 
       <main class="content"></main>
 
-      <div class="change-background-btn" onclick="window.location.pathname = 'ZZZ-Blog/ZZZ Blog/ChangeDynamicWallpaper.html';">
+      <!-- Button to swap footer -->
+      <div class="change-background-btn" id="change-footer-btn">
           <div class="change-background-btn-extra">
               <img class="change-background-img" src="ASSETS/IMG/UI/ChangeWallpaper.png">
           </div>
       </div>
 
-      <?php include 'footer.html' ?>
-
+      <!-- Footer container -->
+      <div id="footer-container">
+          <?php include 'footer.html'; ?>
+      </div>
   </div>
+
+  <script>
+  // Keep track of current footer state
+  let isDynamicFooter = false;
+
+  // Cache main footer HTML
+  const footerContainer = document.getElementById('footer-container');
+  const mainFooterHTML = footerContainer.innerHTML;
+
+  const video = document.getElementById('DynamicWallpaper');
+  const source = video.querySelector('source');
+
+  document.getElementById('change-footer-btn').addEventListener('click', function() {
+      if (!isDynamicFooter) {
+          // Load dynamic footer
+          fetch('CDWfooter.html')
+              .then(response => response.text())
+              .then(html => {
+                  footerContainer.innerHTML = html;
+                  isDynamicFooter = true;
+
+                  // Attach video-changing listeners
+                  const buttons = document.querySelectorAll('#footer-container .footer-scroll .btn');
+                  buttons.forEach(btn => {
+                      btn.addEventListener('click', () => {
+                          const id = btn.id;
+                          source.src = `ASSETS/DynamicWallpapers/DynamicWallpaper${id}.mp4`;
+                          video.load();
+                          video.play();
+                      });
+                  });
+              })
+              .catch(err => console.error('Failed to load footer:', err));
+      } else {
+          // Switch back to main footer
+          footerContainer.innerHTML = mainFooterHTML;
+          isDynamicFooter = false;
+      }
+  });
+  </script>
 </body>
 </html>
